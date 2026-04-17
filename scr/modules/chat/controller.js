13b91fbe4776b9328cda89de5ab26d663ce2d91a
NMDang-AI <nmdang6905@gmail.com>
2026-04-17 22:42:23 +0700

    Update controller.js

--- scr/modules/chat/controller.js
+++ scr/modules/chat/controller.js
@@ -1,1 +1,34 @@
+const chatService = require('./service');
 
+async function sendChat(req, res, next) {
+  try {
+    const result = await chatService.sendChat(req.body, req.user);
+    return res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function getChatHistory(req, res, next) {
+  try {
+    const history = await chatService.getChatHistory(req.user.id);
+    return res.json(history);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function clearChatHistory(req, res, next) {
+  try {
+    await chatService.clearChatHistory(req.user.id);
+    return res.status(204).end();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = {
+  sendChat,
+  getChatHistory,
+  clearChatHistory,
+};
